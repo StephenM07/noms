1db3f8569bff497edc82a1b3ad918012cc6ca4e6
Stephen Macias <stephenmacias@yahoo.com>
2019-07-13 16:35:26 +0000

    noms 30 update

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_04_030347) do
+ActiveRecord::Schema.define(version: 2019_07_10_155851) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_07_04_030347) do
     t.integer "place_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
+    t.integer "user_id"
   end
 
   create_table "places", force: :cascade do |t|
